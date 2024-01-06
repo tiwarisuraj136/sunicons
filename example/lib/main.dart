855a388ff216dde1a56f324645894b99d79fcb7e
LENOVO <paarthbhansali@gmail.com>
2024-01-06 15:40:33 +0530

    used feather icon class 6.1.23 time 3.40pm

---
+++ example/lib/main.dart
@@ -0,0 +1,52 @@
+import "package:flutter/material.dart";
+
+import './icons_list.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  Widget iconCard(IconData iconData, String iconName) {
+    return Card(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            iconData,
+            size: 50.0,
+          ),
+          const SizedBox(height: 30.0),
+          Text(iconName)
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Sun Icons Example",
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF9F9F9),
+        appBar: AppBar(
+          title: const Text('Sun Icons'),
+        ),
+        body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 8.0,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return iconCard(
+              iconDataList[index],
+              iconNameList[index],
+            );
+          },
+          itemCount: iconDataList.length,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/icon_data.dart
@@ -0,0 +1,12 @@
+library sunicons;
+
+import "package:flutter/widgets.dart";
+
+class SunIconData extends IconData {
+  const SunIconData(int suryaPoint)
+      : super(
+          suryaPoint,
+          fontFamily: "SunIcons",
+          fontPackage: "sunicons",
+        );
+}
